9f704ad5d5d247a99cb3a6324e3f5597bfe14583
Ksawier Malkiewicz <ksawier.malkiewicz@icloud.com>
2023-05-30 00:37:39 +0200

    Made a basic gui

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [ method, setMethod ] = useState(0)
+
+  const handleFormSubmit = () => {
+
+  }
+
+  return (
+    <div className="root">
+      <div className="container">
+        <div className="inputArea">
+          <form>
+            <select name="method" value={method} onChange={(e) => {setMethod(parseInt(e.target.value, 10))}}>
+              <option value={0}>GET</option>
+              <option value={1}>POST</option>
+            </select>
+            <input type="text"  />
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+        <div className="result">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
